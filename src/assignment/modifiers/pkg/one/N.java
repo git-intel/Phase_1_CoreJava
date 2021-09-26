9d25f7871474831b5918781da88d2b5f5368d586
ajay augustine <mail.intelegence@gmail.com>
2021-09-26 17:42:57 +0530

    assignment_modifiers--progressing

--- src/assignment/modifiers/pkg/one/N.java
+++ src/assignment/modifiers/pkg/one/N.java
@@ -4,4 +4,9 @@ public class N {
 	protected long var_n_l = 11233232323l;
 	public  int var_n_i = 100;
 	protected float var_n_f = 33.122f;
+	
+	 public void printProtectedVariables() {
+		 System.out.println("protected long of class N: " + var_n_l);
+		 System.out.println("protected float of class N: " + var_n_f);
+	 }
 }
--- src/assignment/modifiers/pkg/two/Z.java
+++ src/assignment/modifiers/pkg/two/Z.java
@@ -1,11 +1,27 @@
 package assignment.modifiers.pkg.two;
 import assignment.modifiers.pkg.one.M;
+import assignment.modifiers.pkg.one.N;
 
 public class Z extends M {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Z objZ = new Z();
+		objZ.printPrivateVariables();
+		objZ.printDefaultVariables();
+		System.out.println("protected float of class M:" + objZ.var_m_f);
 
+		N objN = new N();
+		System.out.println("public int of class N:" + objN.var_n_i);
+		objN.printProtectedVariables();
+		
+		X objX = new X();
+		objX.printPrivateVariables();
+		System.out.println("default long of class x:" + objX.var_x_l);
+		System.out.println("protected float of class x:" + objX.var_x_f);
+		System.out.println("public char of class x:" + objX.var_x_c);
+		
 	}
+
 
 }
